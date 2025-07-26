4038646015b484c058c635aa811c55d6adcac86d
Yaahyaa <134010146+yaahyaa@users.noreply.github.com>
2025-07-26 14:47:02 +0100

    typed.js

--- typed.js
+++ typed.js
@@ -1,6 +1,6 @@
 // typed.js: Handles typing animation for the dedication sentence
 document.addEventListener("DOMContentLoaded", function() {
-    const text = "this heart is for you :) \n     Ayyyaaaaa";
+    const text = "this heart is for you :) \n      Ayyyaaaaa";
     const target = document.getElementById("typed-text");
     let i = 0;
     function typeWriter() {
